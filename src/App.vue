338bee1785a742cd1833ec37a70ce01a5d901832
IOneB <barabanovivan@bk.ru>
2022-01-15 15:42:02 +0400

    init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input v-model="url" />
+    <button @click="fetch">фетч!</button>
+  </div>
+  <div>Данные: {{ data }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+      url: "https://little-interview-helper.herokuapp.com/api/HumanResource",
+    };
+  },
+  methods: {
+    async fetch() {
+      try {
+        const resp = await fetch(this.url);
+        this.data = await resp.text();
+      } catch (error) {
+        this.data = error;
+      }
+    },
+  },
+};
+</script>
